f0721e8666d6b3f53ece19a4e3d5252a8e715968
Simran Khosla <email.simrankhosla@gmail.com>
2015-11-01 14:29:39 -0600

    changed beatles canvas background

--- Canvas-BoilerPlate/beatles/app.js
+++ Canvas-BoilerPlate/beatles/app.js
@@ -1,13 +1,14 @@
-window.onload = function() {
-
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 //get random integer function
+
+window.onload = function() {
 
 
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
+
 //loads the canvas
     var johnValue = getRandomInt(0,200);
     var paulValue = getRandomInt(0,200);
@@ -17,28 +18,28 @@ window.onload = function() {
 
     var johnimg = new Image();
     johnimg.onload = function() {
-    ctx.drawImage(johnimg, 50, johnValue);
+    ctx.drawImage(johnimg, 60, johnValue);
     }
     johnimg.src = "http://i.imgur.com/RsnqEqE.png";
     var paulimg = new Image();
 // loads John
 
     paulimg.onload = function() {
-    ctx.drawImage(paulimg, 150, paulValue);
+    ctx.drawImage(paulimg, 160, paulValue);
     }
     paulimg.src = "http://i.imgur.com/SJ8lGKd.png";
 //loads paul
 
     var georgeimg = new Image();
     georgeimg.onload = function() {
-    ctx.drawImage(georgeimg, 250, georgeValue);
+    ctx.drawImage(georgeimg, 260, georgeValue);
     }
     georgeimg.src = "http://i.imgur.com/fclxApe.png";
 //loads george
 
     var ringoimg = new Image();
     ringoimg.onload = function() {
-    ctx.drawImage(ringoimg, 350, ringoValue);
+    ctx.drawImage(ringoimg, 360, ringoValue);
     }
     ringoimg.src = "http://i.imgur.com/K5SVUrZ.png";
 //loads ringo
@@ -47,15 +48,18 @@ var beatlesMax = Math.max(johnValue, paulValue, georgeValue, ringoValue);
 var beatlesMin = Math.min(johnValue, paulValue, georgeValue, ringoValue);
 //calculate the max/min beatles y for each round
 
-
-if (beatlesMax-beatlesMin<30) {
+if (beatlesMax-beatlesMin<50)
+  {
+  ctx.fillStyle = '#BADA55';
+  ctx.fillRect(40,0,420,350);
   var appleimg = new Image();
   appleimg.onload = function() {
-  ctx.drawImage(appleimg, 100 , 40);
+  ctx.drawImage(appleimg, 400 , 250);
   }
-  appleimg.src = "http://i.imgur.com/viEet9O.png";;
+  appleimg.src = "http://i.imgur.com/B6xUcOa.png";
   };
-  }
+  console.log('you win!');
+}
 //loads the logo if the Beatles are in a row
 
 //the end
